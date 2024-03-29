3aaa4ed2ae96322d1fb2e2222f700b9bd990c513
web <weibozheng@wonder4studio.com>
2024-03-29 17:54:31 +0800

    feat: update baseURL

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,7 @@ export default defineNuxtConfig({
   modules: ['nuxt-icon'],
   devtools: { enabled: true },
   css: ['~/assets/scss/index.scss'],
+  app: {
+    baseURL: process.env.NODE_ENV === 'production' ? '/hexschoolUnderground_7F/' : '/',
+  }
 })
